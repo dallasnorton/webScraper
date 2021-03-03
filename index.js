d8ef3a479953fedc1c0425a117ba8ea009f5af26
Dallas Norton <d.norton@f5.com>
2021-03-03 16:28:48 -0700

    get raw data pretty much

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log("Hello World");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+const main = async () => {
+  const zip = "84043";
+
+  const browser = await puppeteer.launch({
+    // headless: false,
+    // slowMo: 250, // slow down by 250ms
+  });
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  });
+
+  await page.goto("https://www.extraspace.com/");
+  const searchInput = ".search-input-wrapper input";
+
+  await page.click(searchInput);
+  await page.keyboard.type(zip);
+  await page.keyboard.press("Enter");
+
+  await page.waitForNavigation();
+  await page.screenshot({ path: "output/screenshot.png" });
+
+  const facilitiesList = await page.$$("#results-in [data-qa=facility-card]");
+
+  const getFacilityInfo = async () => {
+    return Promise.all(
+      facilitiesList.map(async (facility) => {
+        return await facility.evaluate((node) => {
+          const name = node.querySelector(".fn.org").innerText || "";
+          const address = node.querySelector(".adr").innerText || "";
+          const dataPrice = node.getAttribute("data-price") || "";
+          return { name, address, price: dataPrice };
+        });
+      })
+    );
+  };
+  const facilities = await getFacilityInfo();
+
+  const data = {
+    zip: zip,
+    facilities,
+  };
+
+  await fs.writeFileSync(
+    "output/response.json",
+    JSON.stringify(data, null, "\t")
+  );
+
+  await browser.close();
+};
+
+main();
